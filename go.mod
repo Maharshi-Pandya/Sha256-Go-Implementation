106b837bce4090779336e78fc7c324dd58e3dfd9
Maharshi Pandya <maharshi.pandya2002@gmail.com>
2021-10-30 17:55:14 +0530

    :tada: Saturday 30 October 2021 05:54:51 PM IST

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/sha
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"example.com/sha/shaimpl"
+)
+
+func main() {
+	sha := shaimpl.Sha256{}
+
+	var str string
+
+	if len(os.Args) < 2 {
+		str = ""
+	} else {
+		str = os.Args[1]
+	}
+
+	// calc the hash
+	hash := sha.Hash(str)
+
+	// print the byte array and hexstring
+	fmt.Println(hash)
+	fmt.Println(shaimpl.ToHexString(hash))
+}
